7f23b4254d3e957e48c1425261952389a141faae
VarshaS-eng <varunvarsha85@gmail.com>
2020-01-28 11:12:23 +0530

    JSON fetch

--- src/components/onlineposts/getonline.js
+++ src/components/onlineposts/getonline.js
@@ -1,112 +1,136 @@
-/* eslint-disable no-undef */
 import React, { Component } from 'react';
 import './getonline.css'
-// eslint-disable-next-line no-unused-vars
-import _  from 'lodash';
+import _ from 'lodash';
 
-// it's return a json file
+
+import Pagination from "react-pagination-js";
+import "react-pagination-js/dist/styles.css";
+
+
 class GetOnlinePosts extends Component {
-    constructor(props){
-        super(props);
-        this.state = {
-            error : null,
-            isLoaded : false,
-            posts : []          
-        };
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      //activePage: 10,
+      currentPage: 1,
+      error: null,
+      isLoaded: false,
+      posts: [],
+      // length: 0
+    };
 
-    componentDidMount(){
-        // I will use fake api from jsonplaceholder website
-        // this return 100 posts 
-        fetch("https://jsonplaceholder.typicode.com/todos")
-        .then( response => response.json())
-        .then(
-            // handle the result
-            (result) => {
-                 //iteratees = obj => obj.completed;
-               // const sorted = _.sortBy(result, obj => obj.completed);
-               var partition = _.partition (result, obj => obj.completed,true) 
-              console.log(partition)
-                this.setState({
-                    isLoaded : true,
-                    posts : partition
-                })
-            
-            },
-        
+  }
 
-            // Handle error 
-            (error) => {
-                this.setState({
-                    isLoaded: true,
-                    error
-                })
-            },
-        )
-    }
+  /*handlePageChange = (pageNumber) => {
+    console.log(`active page is ${pageNumber}`);
 
-    render() {
-        const {error, isLoaded, posts} = this.state;
-        
-        const posts1 = posts[0];
-        const posts2 = posts[1];
-        // console.log(posts[0])
-        if(error){
-            return <div>Error in loading</div>
-        }else if (!isLoaded) {
-            return <div>Loading ...</div>
-        }else{
-            return(
-                <div>
-                    <ol className="item">
-                    {
-                        posts1.map(post => (
-                            <div> 
-                            
-                            <li key={post.id} align="start">
-                    
-                                <div>
-                                     
-                                    <input type="checkbox"  defaultChecked={post.completed}/>
-                                    <span className="title">{post.title}</span>
-                                    <span className="body">{post.body}</span>
-                                </div>
-                            </li>
-                            
-                        </div>
+    this.setState({ activePage: pageNumber });
+  }*/
 
-                        ))
-                    }
-                    
-                    </ol>
-            
-            
-                <ol className="item">
-                {
-                    posts2.map(post => (
-                        <div> 
-                        
-                        <li key={post.id} align="start">
-                
-                            <div>
-                                 
-                                <input type="checkbox"  defaultChecked={post.completed}/>
-                                <span className="title">{post.title}</span>
-                                <span className="body">{post.body}</span>
-                            </div>
-                        </li>
-                        
+  changeCurrentPage = numPage => {
+    this.setState({ currentPage: numPage });
+    //fetch a data
+    //or update a query to get data
+  };
+
+  componentDidMount() {
+    // I will use fake api from jsonplaceholder website
+    // this return 100 posts 
+    fetch("https://jsonplaceholder.typicode.com/todos")
+      .then(response => response.json())
+      .then(
+        // handle the result
+        (result) => {
+          console.log(result)
+          //iteratees = obj => obj.completed;
+          // const sorted = _.sortBy(result, obj => obj.completed);
+          var partition = _.partition(result, obj => obj.completed)
+          this.setState({
+            isLoaded: true,
+            posts: partition,
+          })
+
+        })
+      .catch(error => {
+        this.setState({
+          isLoaded: true,
+          error
+        })
+      }
+      )
+  }
+
+  render() {
+
+    const { error, isLoaded, posts } = this.state;
+
+    console.log(this.state)
+    // console.log(posts[0])
+    if (error) {
+      return <div>Error in loading</div>
+    } else if (!isLoaded) {
+      return <div>Loading ...</div>
+    } else {
+      const posts1 = posts[0];
+
+      const posts2 = posts[1];
+      console.log("Length of the posts" + posts1.length );
+      return (
+
+        <div>
+          <div>
+            <ol className="item" >
+              {
+                posts1.map(post => (
+                  <li key={post.id} align="start">
+                    <div>
+                      <input type="checkbox" defaultChecked={post.completed} />
+                      <span className="title">{post.title}</span>
+                      <span className="body">{post.body}</span>
                     </div>
 
-                    ))
-                }
-                
-                </ol>
-            </div>
-            );
-        }
-      
+                  </li>
+
+                ))
+              }
+
+            </ol>
+            <Pagination
+            
+              currentPage={this.state.currentPage}
+              totalSize={posts1.length}
+              
+              totalPages={10}
+              changeCurrentPage={this.changeCurrentPage}
+            />
+            <h2>current Page:{this.state.currentPage}</h2>
+          </div>
+
+          <ol className="item">
+            {
+              posts2.map(post => (
+                <li key={post.id} align="start">
+                  <div>
+                    <input type="checkbox" defaultChecked={post.completed} />
+                    <span className="title">{post.title}</span>
+                    <span className="body">{post.body}</span>
+                  </div>
+                </li>
+              ))
+            }
+          </ol>
+
+          <Pagination
+            currentPage={this.state.currentPage}
+            totalPages={10}
+            changeCurrentPage={this.changeCurrentPage}
+          />
+          <h2>current Page:{this.state.currentPage}</h2>
+        </div>
+
+
+      );
     }
   }
-  
-  export default GetOnlinePosts;
+}
+export default GetOnlinePosts;
